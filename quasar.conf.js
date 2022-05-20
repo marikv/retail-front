d55cc6f755fe0160f16ad0d9fd6b92b1df6f9d31
Marin <marin.vartan@gmail.com>
2022-05-20 18:20:27 +0300

    b

--- quasar.conf.js
+++ quasar.conf.js
@@ -152,7 +152,7 @@ module.exports = configure((ctx) => ({
       display: 'standalone',
       orientation: 'portrait',
       background_color: '#ffffff',
-      theme_color: '#027be3',
+      theme_color: '#5539CC',
       icons: [
         {
           src: 'icons/icon-128x128.png',
